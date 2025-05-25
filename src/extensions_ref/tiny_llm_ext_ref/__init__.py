aad708d0b36a493e9a92888aeaf4c449bdf14e1f
Alex Chi <iskyzh@gmail.com>
2025-05-25 23:01:58 +0800

    handle import error for C++ extensions
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/extensions_ref/tiny_llm_ext_ref/__init__.py
+++ src/extensions_ref/tiny_llm_ext_ref/__init__.py
@@ -1,9 +1,13 @@
 # Copyright © 2023 Apple Inc.
 
-import mlx.core as mx
-
-from ._ext import *
 from pathlib import Path
 
-current_path = Path(__file__).parent
-load_library(mx.gpu, str(current_path))
+import mlx.core as mx
+
+try:
+    from ._ext import *
+
+    current_path = Path(__file__).parent
+    load_library(mx.gpu, str(current_path))
+except ImportError:
+    print("Failed to load C++/Metal extension")
